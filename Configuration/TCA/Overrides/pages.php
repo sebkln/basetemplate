394fbdf9902d3780fd6bbf8744515e5a429f2fbd
Sebastian Klein <laitnin@gmx.net>
2023-02-10 20:10:08 +0100

    [TASK] Update Page and User TSconfig

--- Configuration/TCA/Overrides/pages.php
+++ Configuration/TCA/Overrides/pages.php
@@ -9,37 +9,7 @@ call_user_func(static function () {
 
     ExtensionManagementUtility::registerPageTSConfigFile(
         $extensionKey,
-        'Configuration/TSconfig/Page/BackendLayouts.tsconfig',
-        'Template Extension: BackendLayouts'
-    );
-
-    ExtensionManagementUtility::registerPageTSConfigFile(
-        $extensionKey,
-        'Configuration/TSconfig/Page/mod.tsconfig',
-        'Template Extension: Module TSconfig [mod]'
-    );
-
-    ExtensionManagementUtility::registerPageTSConfigFile(
-        $extensionKey,
-        'Configuration/TSconfig/Page/rte_ckeditor.tsconfig',
-        'Template Extension: Config for Rich Text Editor (CKEditor)'
-    );
-
-    ExtensionManagementUtility::registerPageTSConfigFile(
-        $extensionKey,
-        'Configuration/TSconfig/Page/TCAdefaults.tsconfig',
-        'Template Extension: Default values for records [TCAdefaults]'
-    );
-
-    ExtensionManagementUtility::registerPageTSConfigFile(
-        $extensionKey,
-        'Configuration/TSconfig/Page/TCEFORM.tsconfig',
-        'Template Extension: Form fields TSconfig [TCEFORM]'
-    );
-
-    ExtensionManagementUtility::registerPageTSConfigFile(
-        $extensionKey,
-        'Configuration/TSconfig/Page/TCEMAIN.tsconfig',
-        'Template Extension: Page permissions et al. [TCEMAIN]'
+        'Configuration/TSconfig/Page.tsconfig',
+        'Template Extension: Page TSconfig'
     );
 });
